1353f8ef5004d6f0d33b5839fe5ca0e868792817
Grayman1 <54072430+Grayman1@users.noreply.github.com>
2021-07-15 20:46:00 +0000

    Correct errors. Code working

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
+// From YouTube tutorial by Ania Kubow
+// https://github.com/kubowania
+
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left')
 let score = document.querySelector('#score')
 
 let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
 
 function randomSquare() {
+  squares.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPartition = squares[Math.floor(Math.random() * 9)]
+  randomPartition.classList.add('mole')
 
 
-
-  
+  // Assign the id of the randomPartition to hitPosition for us to use later
+  hitPosition = randomPartition.id  
 }
 
+squares.forEach(id => {
+  id.addEventListener('mousedown', () => {
+    if(id.id === hitPosition) {
+      result = result + 1
+      score.textContent = result
+      hitPosition = null
+    }
+  })
+})
 
+function moveMole() {
+  let timerId = null
+  timerId = setInterval(randomSquare, 1000)
+}
 
+moveMole()
 
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
 
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId)
+    clearInterval(timerId)
+    alert('GAME OVER! Your final score is: ' + result)
+  }
 
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
